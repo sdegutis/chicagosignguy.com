0c61655d2ad3da6d244f23d26b138559cdc2afca
Steven Degutis <sbdegutis@gmail.com>
2026-01-05 14:12:49 -0600

    Cache. Ugh.

--- site/build.tsx
+++ site/build.tsx
@@ -50,7 +50,7 @@ function Html(attrs: { title: string, children: any }) {
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>{attrs.title}</title>
-        <link rel="stylesheet" href="/css/base.css" />
+        <link rel="stylesheet" href={"/css/base.css?" + Date.now()} />
 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin='' />
