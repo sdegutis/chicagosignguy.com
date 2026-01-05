93d1a11cc8f7fba606f90b1bda8f3f655f9c146e
Steven Degutis <sbdegutis@gmail.com>
2026-01-05 12:22:01 -0600

    Home.

--- site/template/home.tsx
+++ site/template/home.tsx
@@ -2,6 +2,8 @@ import { type Blog } from "../build.ts"
 
 export function RenderHome(blogs: Blog[]) {
   return <>
+    <h1>Chicago Sign Guy (Steven)</h1>
+    <p><img src="/img/me.jpg" /></p>
     <ul>
       {blogs.map(blog => <>
         <li><a href={blog.path}>{blog.title}</a></li>
