47ed96e1115fdc768d38a5ec3923d13c1c2c383c
Steven Degutis <sbdegutis@gmail.com>
2026-01-05 12:30:11 -0600

    Css.

--- site/template/common.tsx
+++ site/template/common.tsx
@@ -1,5 +1,16 @@
 export function Html(attrs: { title: string, children: any }) {
   return <>
-    {attrs.children}
+    {'<!DOCTYPE html>'}
+    <html lang="en">
+      <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>{attrs.title}</title>
+        <link rel="stylesheet" href="/css/base.css" />
+      </head>
+      <body>
+        {attrs.children}
+      </body>
+    </html>
   </>
 }
