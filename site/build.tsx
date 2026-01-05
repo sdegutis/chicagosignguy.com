c778abad855e39bd2330b10581f0baef624c0782
Steven Degutis <sbdegutis@gmail.com>
2026-01-05 13:40:53 -0600

    More goodly.

--- site/build.tsx
+++ site/build.tsx
@@ -54,7 +54,7 @@ function Html(attrs: { title: string, children: any }) {
 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin='' />
-        <link href="https://fonts.googleapis.com/css2?family=Crimson+Text:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&display=swap" rel="stylesheet"></link>
+        <link href="https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400..800;1,400..800&family=Libre+Caslon+Text:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet"></link>
 
       </head>
       <body>
