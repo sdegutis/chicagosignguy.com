4753916a37a4e6fcb47cf721669636e910868937
Steven Degutis <sbdegutis@gmail.com>
2026-01-06 17:35:13 -0600

    Title.

--- site/build.tsx
+++ site/build.tsx
@@ -55,7 +55,7 @@ function Html(attrs: { title: string, children: any }) {
 
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>{attrs.title}</title>
+        <title>{attrs.title} - Chicago Sign Guy's Website</title>
         <link rel="stylesheet" href={"/css/base.css?" + Date.now()} />
 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
