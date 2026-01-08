ff99edd380d7dcc6976f59e9ad4606148e86b817
Steven Degutis <sbdegutis@gmail.com>
2026-01-08 08:53:57 -0600

    More.

--- site/build.tsx
+++ site/build.tsx
@@ -246,6 +246,20 @@ function AllArticles(data: { blogs: Blogs, blog?: Blog, tag?: string }) {
         </>)}
       </ul>
     </>)}
+    <H>Planned</H>
+    <ul class='articles'>
+      <li><span />Survey: How alone do you feel?</li>
+      <li><span />Survey: How did you meet your friends?</li>
+      <li><span />Article: Anxiety, confidence, fear, arrogance</li>
+      <li><span />Article: Love, purpose, burnout, motivation</li>
+      <li><span />Experiment: Live upvote/downvote</li>
+      <li><span />Experiment: Podcast episode with 100 cohosts</li>
+      <li><span />Experiment: Free 3 minute venting or trauma dumping</li>
+      <li><span />Discussion: What is masculinity?</li>
+      <li><span />Discussion: What is femininity?</li>
+      <li><span />Discussion: How do you fall in love?</li>
+      <li><span />Discussion: Can romantic love be permanently celibate?</li>
+    </ul>
   </>
 }
 
