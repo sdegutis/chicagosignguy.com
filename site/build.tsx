9a811c317db4f42e999bfcfff5058259dca1b4b3
Steven Degutis <sbdegutis@gmail.com>
2026-01-08 14:03:16 -0600

    Church.

--- site/build.tsx
+++ site/build.tsx
@@ -234,10 +234,11 @@ function PlaylistPage() {
 
     <ul class='articles'>
       <li><span /><i>a french girl singing <a href='https://www.youtube.com/watch?v=QwoF1-1QgwA'>je te laisserai des mots</a> while it's raining</i></li>
-      <li><span /><i>the only good <a href="https://www.youtube.com/watch?v=J_sH-GrUeUw&t=22s">creep</a> performance by radiohead</i></li>
-      <li><span /><i>aaron lewis singing <a href="https://www.youtube.com/watch?v=EEaLxw3Gpp8">black</a> by pearl jam</i></li>
+      <li><span /><i>the only actually good <a href="https://www.youtube.com/watch?v=J_sH-GrUeUw&t=22s">creep</a> live performance by radiohead</i></li>
+      <li><span /><i>aaron lewis's passionate acoustic cover of <a href="https://www.youtube.com/watch?v=EEaLxw3Gpp8">black</a> by pearl jam</i></li>
       <li><span /><i>emotional cover of <a href='https://www.youtube.com/watch?v=nfuYmE1CxP4&t=44s'>exit music (for a film)</a> at a talent show</i></li>
-      <li><span /><i>unique version of <a href='https://www.youtube.com/watch?v=ze5fxkUcpbc&t=1959s'>king of glory, king of peace</a> sung during Mass</i></li>
+      <li><span /><i>passionate rendition of <a href='https://www.youtube.com/watch?v=1nnRC6jDOCI'>take me to church</a> in an nyc subway</i></li>
+      <li><span /><i>unique version of <a href='https://www.youtube.com/watch?v=ze5fxkUcpbc&t=1959s'>King of glory, King of peace</a> sung during Mass</i></li>
     </ul>
 
   </Html>
